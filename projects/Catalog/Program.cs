0612ca486f976e577efbbd8468aadad6fa931714
Niki Najafi <you@example.com>
2023-10-23 15:34:47 -0400

    added MongoDB and all the REQUESTS 2:00

--- projects/Catalog/Program.cs
+++ projects/Catalog/Program.cs
@@ -1,10 +1,27 @@
 using Catalog.Repositories;
+using Catalog.Settings;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
+
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
 
 // Add services to the container.
 //Register dependency
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+
+//Whenever there is GUID serialize it into string
+BsonSerializer.RegisterSerializer(new GuidSerializer(representation: BsonType.String));
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(representation: BsonType.String));
+
+builder.Services.AddSingleton<IMongoClient>(ServiceProvider => {
+    var settings = builder.Configuration.GetSection("MongoDbSettings").Get<MongoDbSettings>();
+    return new MongoClient(settings?.ConnectionString);
+});
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
